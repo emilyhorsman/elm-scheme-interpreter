7674de635650881f215a3a743c7a8aa152b590e8
Emily Horsman <me@emilyhorsman.com>
2016-12-24 18:20:48 -0500

    Add textarea input in Main

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,55 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, textarea, text, div)
+import Html.Attributes exposing (value, style)
+import Html.Events exposing (onInput)
 import Lexer exposing (tokenize)
 
 
-source =
-    "(!input+ ())"
+type alias Model
+    = String
+
+
+model : Model
+model =
+    "()"
+
+
+type Msg
+    = Change String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change text ->
+            text
+
+
+view : Model -> Html Msg
+view model =
+    div
+        []
+        [ textarea
+            [ onInput Change
+            , value model
+            , style
+                [ ( "font-family", "monospace" )
+                , ( "width", "50%" )
+                , ( "height", "40vh" )
+                ]
+            ]
+            []
+        , div
+            []
+            [ model |> tokenize |> toString |> text
+            ]
+        ]
 
 
 main =
-    source
-        |> tokenize
-        |> toString
-        |> text
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
